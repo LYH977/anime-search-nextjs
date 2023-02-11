77c69824e58dd6e2aa4f8e7e49913fa7b2f2ce4c
Lee Yuan Hooi <ho0i97@hotmail.com>
2023-02-11 23:23:40 +0800

    temporaru inline svg

--- components/Pagination/index.tsx
+++ components/Pagination/index.tsx
@@ -17,16 +17,16 @@ export const Pagination = (props: PaginationProps) => {
     <nav aria-label="Page navigation">
       <ul className="inline-flex items-center -space-x-px">
         { totalPages > 5 && currentPage > 1 && <li>
-          <a href='#' className="block px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700" onClick={ goToPrevPage }>
+          <a href='#' className="isolate sblock px-3 py-2 ml-0 leading-tight text-gray-500 bg-white border border-gray-300 rounded-l-lg hover:bg-gray-100 hover:text-gray-700" onClick={ goToPrevPage }>
             <span className="sr-only">Previous</span>
-            <PrevArrow />
+            { prev }
           </a>
         </li> }
         <PageNumbers { ...props } />
         { totalPages > 5 && currentPage < totalPages && <li>
-          <a href='#' className="block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700" onClick={ goToNextPage }>
+          <a href='#' className="isolate block px-3 py-2 leading-tight text-gray-500 bg-white border border-gray-300 rounded-r-lg hover:bg-gray-100 hover:text-gray-700" onClick={ goToNextPage }>
             <span className="sr-only">Next</span>
-            <NextArrow />
+            { next }
           </a>
         </li> }
       </ul>
@@ -34,3 +34,6 @@ export const Pagination = (props: PaginationProps) => {
   )
 }
 
+const prev = <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clipRule="evenodd"></path></svg>
+
+const next = <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clipRule="evenodd"></path></svg>
