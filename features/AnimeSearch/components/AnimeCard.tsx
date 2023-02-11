31ecdbee7b2d935cfdfed81e93127aca25ad9792
Lee Yuan Hooi <ho0i97@hotmail.com>
2023-02-11 21:04:25 +0800

    used normal onclick on ANimeCard to prevent prefetch on Link Component 2

--- features/AnimeSearch/components/AnimeCard.tsx
+++ features/AnimeSearch/components/AnimeCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import Link from 'next/link';
+import { useRouter } from 'next/router'
 
 type AnimeCardProps = {
     title: string;
@@ -11,14 +11,19 @@ type AnimeCardProps = {
 
 export const AnimeCard = ({ title, id, imageUrl }: AnimeCardProps) => {
     const animeId = 'anime-' + id
+    const router = useRouter()
+    const navigateToDetailPage = () => {
+        router.push(`/anime/${id}`, undefined, { shallow: true })
+    }
+
     return (
-        <Link href={ `/anime/${id}` } className='shadow-lg shadow-[#2e3d0d] pb-4 w-[200px] h-[400px] rounded-lg overflow-hidden cursor-pointer ' tabIndex={ 0 } title={ title } aria-labelledby={ animeId }>
+        <div className='shadow-lg shadow-[#2e3d0d] pb-4 w-[200px] h-[400px] rounded-lg overflow-hidden cursor-pointer ' tabIndex={ 0 } title={ title } aria-labelledby={ animeId } onClick={ navigateToDetailPage }>
 
             <div className='overflow-hidden'>
                 <Image src={ imageUrl } alt={ title } width={ 200 } height={ 300 } className='object-cover h-[300px] isolate hover:scale-105 transition-transform' />
             </div>
             <p id={ animeId } className='px-2 pt-2 line-clamp-2 overflow-hidden'>{ title }</p>
-        </Link>
+        </div>
     )
 }
 
