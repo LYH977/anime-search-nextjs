fc818cd95498050d016d4aaebc1c2c9273eb4b7d
Lee Yuan Hooi <ho0i97@hotmail.com>
2023-02-12 17:18:47 +0800

    added displayName to Searchbar with forwardRef

--- components/SearchBar/index.tsx
+++ components/SearchBar/index.tsx
@@ -5,12 +5,12 @@ import React from 'react'
 
 type SearchBarProps = {
     isLoading: boolean
-} & React.ComponentPropsWithRef<'input'>
+} & React.ComponentProps<'input'>
 
 
 
 
-export const SearchBar = React.forwardRef(({ isLoading, ...rest }: SearchBarProps, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
+const SearchBar = React.forwardRef(({ isLoading, ...rest }: SearchBarProps, ref: React.LegacyRef<HTMLInputElement> | undefined) => {
     return (
         <div className='relative w-full'>
             { isLoading && <div role='status'>
@@ -24,3 +24,7 @@ export const SearchBar = React.forwardRef(({ isLoading, ...rest }: SearchBarProp
             <input ref={ ref } type="search" id="default-search" className="block p-4 w-full pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-100 focus-visible:ring-blue-500 focus-visible:border-blue-500 outline-none" { ...rest } />
         </div>)
 })
+
+SearchBar.displayName = 'SearchBar';
+
+export { SearchBar }
